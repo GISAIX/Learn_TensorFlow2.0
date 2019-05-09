e33f58fde6ccf8db46bc4f8979511cb35b0b5bf6
stdcoutzrh <stdcoutzrh@gmail.com>
2019-05-09 11:48:50 +0800

    Add cat vs dog

---
+++ Keras/Keras_6.Finetune_VGG16_catVSdog.py
@@ -0,0 +1,45 @@
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPool2D,Activation,Dropout,Flatten,Dense
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator,img_to_array,load_img
+
+VGG16_model = VGG16(weights='imagenet',include_top=False, input_shape=(150,150,3))
+
+top_model = Sequential()
+top_model.add(Flatten(input_shape=VGG16_model.output_shape[1:]))
+top_model.add(Dense(256,activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(2,activation='softmax'))
+
+model = Sequential()
+model.add(VGG16_model)
+model.add(top_model)
+
+train_datagen = ImageDataGenerator(
+    rotation_range = 40,width_shift_range = 0.2,height_shift_range = 0.2, rescale = 1/255,shear_range = 20,
+    zoom_range = 0.2,horizontal_flip = True,fill_mode = 'nearest',) 
+test_datagen = ImageDataGenerator(rescale = 1/255,) # 数据归一化 
+
+batch_size = 32
+
+# train_data
+train_generator = train_datagen.flow_from_directory(
+    'Keras\data\catVSdog/train',
+    target_size=(150,150),
+    batch_size=batch_size)
+
+# test_data
+test_generator = test_datagen.flow_from_directory(
+    'Keras\data\catVSdog/test',
+    target_size=(150,150),
+    batch_size=batch_size )
+
+# train
+model.compile(optimizer=SGD(lr=1e-4,momentum=0.9),loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit_generator(train_generator,steps_per_epoch=len(train_generator),
+                    epochs=50,validation_data=test_generator,
+                    validation_steps=len(test_generator))
+
+model.save('Keras\keras_app\catVSdog\models\model_vgg16.h5')
