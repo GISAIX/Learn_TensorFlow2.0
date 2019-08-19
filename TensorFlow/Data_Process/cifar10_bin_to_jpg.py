bde766cdc4d35c7efb973791b6dca5f05ad15459
stdcoutzrh <stdcoutzrh@gmail.com>
2019-08-19 10:53:25 +0800

    Add bin to jpg

--- TensorFlow/Data_Process/cifar10_bin_to_jpg.py
+++ TensorFlow/Data_Process/cifar10_bin_to_jpg.py
@@ -14,7 +14,7 @@ def unpickle(file):
 data  = []
 labels = []
 for i in range(1,6):
-    file_name = "train_data/data_batch_"+str(i)
+    file_name = r"TensorFlow\Data_Process\cifar-10-python\data_batch_"+str(i)
     result = unpickle(file_name)
     data += list(result[b"data"])
     labels += list(result[b"labels"])
@@ -23,14 +23,15 @@ for i in range(1,6):
 imgs = np.reshape(data, [-1, 3, 32, 32])
 
 for i in range(imgs.shape[0]):
+    print("processing image {}.".format(i))
     im_data = imgs[i, ...]
     im_data = np.transpose(im_data, [1, 2, 0])
     im_data = cv.cvtColor(im_data, cv.COLOR_RGB2BGR)
     
-    f = "{}/{}".format("train_data/cifar10_jpgs", classes[labels[i]])
+    f = r"{}\{}".format(r"TensorFlow\Data_Process\images", classes[labels[i]])
 
     if not os.path.exists(f):
-        os.mkdir(f)
+        os.makedirs(f)
 
     cv.imwrite("{}/{}.jpg".format(f, str(i)), im_data)
 
